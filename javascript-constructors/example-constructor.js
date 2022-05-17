120d5a3de31ba2f77fc6af4e7f23b4dcfd50297e
codewithbingle <codewithbingle@gmail.com>
2022-05-17 03:34:21 +0000

    Updated javacript-constructors

--- javascript-constructors/example-constructor.js
+++ javascript-constructors/example-constructor.js
@@ -1,7 +1,7 @@
 function ExampleConstructor() {}
 
 console.log('value of ExampleConstructor.prototype: ', ExampleConstructor.prototype);
-console.log('type of ExampleConstructor.prototype: ', ExampleConstructor.prototype);
+console.log('typeof ExampleConstructor.prototype: ', typeof ExampleConstructor.prototype);
 
 var newExampleConstructor = new ExampleConstructor();
 console.log('value of newExampleConstructor: ', newExampleConstructor);
